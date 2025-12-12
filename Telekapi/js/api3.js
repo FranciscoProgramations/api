78a84b289099b226f5e1532f06c4c0cb33dca355
Francisco <frasa112009@gmail.com>
2025-12-12 00:06:28 -0300

    Update api3.js

--- Telekapi/js/api3.js
+++ Telekapi/js/api3.js
@@ -1,28 +1,74 @@
-chamaPokemon()
+chamaRM()
+chamaGoT()
+chamaST()
 
-async function chamaPokemon(){
+async function chamaRM(){
     try
     {
-        const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/reshiram`)
-        if(!resposta.ok)
-        {
-            throw new Error("Pokemon não encontrado, tente novamente.")
-        }
+        const resposta = await fetch(`https://rickandmortyapi.com/api/character/48`)
         const obj = await resposta.json();
 
-        const sprite = obj.sprites.front_default;
-        const spriteIMG = document.getElementById("sprite");
+        const nome = obj.name
+        const RMnome =document.getElementById("nom")
 
-        spriteIMG.src = sprite;
-        spriteIMG.style.display = "block";
+        const status = obj.status
+        const RMstatus = document.getElementById("stat")
 
-        const dex = obj.id;
-        const numDex = document.getElementById("dex");
-
-        numDex.innerHTML = dex
+        RMnome.innerHTML = "Nome: "+nome
+        RMstatus.innerHTML = "Status: "+status
         
-        console.log(dex)
-        console.log(obj.name)
+    }
+    catch(e)
+    {    alert(e)
+
+    }
+}
+
+async function chamaGoT(){
+    try
+    {
+        const resposta = await fetch(`https://anapioficeandfire.com/api/characters/583`)
+        const obj = await resposta.json();
+
+        const nome2 = obj.name
+        const GoTnome =document.getElementById("nom2")
+
+        const culture = obj.culture
+        const GoTculture =document.getElementById("cultur")
+
+        const titles = obj.titles
+        const GoTtitles = document.getElementById("tit")
+
+        GoTnome.innerHTML = "Nome: "+nome2
+        GoTculture.innerHTML = "Cultura: "+culture
+        GoTtitles.innerHTML = "Títulos: "+titles
+        
+    }
+    catch(e)
+    {    alert(e)
+
+    }
+}
+
+async function chamaST(){
+    try
+    {
+        const resposta = await fetch(`https://swapi.dev/api/planets/1`)
+        const obj = await resposta.json();
+
+        const nome3 = obj.name
+        const STnome = document.getElementById("nom3")
+
+        const clima = obj.climate
+        const STclima = document.getElementById("clima")
+
+        const population = obj.population
+        const STpopulation = document.getElementById("popula")
+
+
+        STnome.innerHTML = "Nome do Planeta: "+nome3
+        STclima.innerHTML = "Clima: "+clima
+        STpopulation.innerHTML = "População: "+population
     }
     catch(e)
     {    alert(e)
